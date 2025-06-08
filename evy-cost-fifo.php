02efa10408f3f377432967508f3a82f6f346dc0e
EvyOfficer <sattawat.sav@gmail.com>
2025-06-08 08:06:45 +0700

    Update evy-cost-fifo.php

--- evy-cost-fifo.php
+++ evy-cost-fifo.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name: Evy Cost FIFO – WooCommerce Inventory Costing & AP Plugin
- * Plugin URI: https://yourwebsite.com/
+ * Plugin Name: Evy - Cost FIFO
+ * Plugin URI: https://github.com/EvyOfficer
  * Description: Manages FIFO inventory costing and Accounts Payable for WooCommerce, with Google Sheets integration.
  * Version: 1.0.0
  * Author: Evy
- * Author URI: https://yourwebsite.com/
+ * Author URI: https://github.com/EvyOfficer
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: evy-cost-fifo
